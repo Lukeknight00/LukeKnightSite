a287c3b5fd9856340b6b55d0c0c33bbf12491fd5
Lukeknight00 <Lukeknight00@gmail.com>
2021-04-30 09:17:46 -0700

    added a plant indexpage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,7 +63,7 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
-              <Link className="navbar-item" to="/plants">
+              <Link className="navbar-item" to="/plant">
                 Plants
               </Link>
               <Link className="navbar-item" to="/plant/Monty">
