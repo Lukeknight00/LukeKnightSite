9f2c974c36ec4552bda5683f23dac74c50ce4ac6
Lukeknight00 <Lukeknight00@gmail.com>
2021-06-01 21:50:57 -0700

    Updated contact page

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -27,7 +27,7 @@ export default class Index extends React.Component {
       body: encode({
         'form-name': form.getAttribute('name'),
         ...this.state,
-      }),
+      }), 
     })
       .then(() => navigate(form.getAttribute('action')))
       .catch((error) => alert(error))
@@ -37,80 +37,90 @@ export default class Index extends React.Component {
     return (
       <Layout>
         <section className="section">
-          <div className="container">
-            <div className="content">
-              <h1>Interested in reaching me?<p style={{fontSize: "70%"}}> Fill out the form below and I will be sure to get back to you ASAP!</p></h1>
-              <form
-                name="contact"
-                method="post"
-                action="/contact/thanks/"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-                onSubmit={this.handleSubmit}
-              >
-                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-                <input type="hidden" name="form-name" value="contact" />
-                <div hidden>
-                  <label>
-                    Don’t fill this out:{' '}
-                    <input name="bot-field" onChange={this.handleChange} />
-                  </label>
+          <div className="container " >
+            <div className="columns">
+             
+              <div className="column is-6">
+                <div className="content">
+
+                  <h1>Interested in reaching me?<p style={{fontSize: "70%"}}> Fill out the form below and I will be sure to get back to you ASAP!</p></h1>
+                  <form
+                    name="contact"
+                    method="post"
+                    action="/contact/thanks/"
+                    data-netlify="true"
+                    data-netlify-honeypot="bot-field"
+                    onSubmit={this.handleSubmit}
+                  >
+                    {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                    <input type="hidden" name="form-name" value="contact" />
+                    <div hidden>
+                      <label>
+                        Don’t fill this out:{' '}
+                        <input name="bot-field" onChange={this.handleChange} />
+                      </label>
+                    </div>
+                    <div className="field">
+                      <label className="label" htmlFor={'name'} >
+                        Your name
+                      </label>
+                      <div className="control">
+                        <input
+                          className="input"
+                          type={'text'}
+                          name={'name'}
+                          onChange={this.handleChange}
+                          id={'name'}
+                          required={true}
+                          style={{minWidth:"20vw", maxWidth:"30vw",}}
+                        />
+                      </div>
+                    </div>
+                    <div className="field">
+                      <label className="label" htmlFor={'email'}>
+                        Email
+                      </label>
+                      <div className="control">
+                        <input
+                          className="input"
+                          type={'email'}
+                          name={'email'}
+                          onChange={this.handleChange}
+                          id={'email'}
+                          required={true}
+                          style={{minWidth:"30vw", maxWidth:"50vw", }}
+                        />
+                      </div>
+                    </div>
+                    <div className="field">
+                      <label className="label" htmlFor={'message'}>
+                        Message
+                      </label>
+                      <div className="control">
+                        <textarea
+                          className="textarea"
+                          name={'message'}
+                          onChange={this.handleChange}
+                          id={'message'}
+                          required={true}
+                          style={{minWidth:"30vw", maxWidth:"70vw", }}
+                        />
+                      </div>
+                    </div>
+                    <div className="field">
+                      <button className="button is-link" type="submit">
+                        Send
+                      </button>
+                    </div>
+                  </form>
                 </div>
-                <div className="field">
-                  <label className="label" htmlFor={'name'} >
-                    Your name
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={'text'}
-                      name={'name'}
-                      onChange={this.handleChange}
-                      id={'name'}
-                      required={true}
-                      style={{minWidth:"20vw", maxWidth:"30vw",}}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={'email'}>
-                    Email
-                  </label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type={'email'}
-                      name={'email'}
-                      onChange={this.handleChange}
-                      id={'email'}
-                      required={true}
-                      style={{minWidth:"30vw", maxWidth:"50vw", }}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label" htmlFor={'message'}>
-                    Message
-                  </label>
-                  <div className="control">
-                    <textarea
-                      className="textarea"
-                      name={'message'}
-                      onChange={this.handleChange}
-                      id={'message'}
-                      required={true}
-                      style={{minWidth:"30vw", maxWidth:"70vw", }}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <button className="button is-link" type="submit">
-                    Send
-                  </button>
-                </div>
-              </form>
+              </div>
+              <div className="column is-6" style={{borderLeft:"black solid 10px",borderTop:"black solid 10px"}}>
+                click to restart
+                <iframe src="https://editor.p5js.org/Lukeknight00/embed/w2MLcLZqW" style={{position: 'relative', width: '100%', height: '100%', minHeight:"70vh"}} ></iframe>
+              </div>
             </div>
-          </div>
+            </div>
         </section>
       </Layout>
     )
